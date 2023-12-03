9bb65ac6cc089315e9554dba1461442f1a95f555
Tibet Tornaci <oofdere@hey.com>
2023-12-03 00:10:46 -0800

    [aoc2023] day3 pt2

--- AdventOfCode/2023/03/src/main.rs
+++ AdventOfCode/2023/03/src/main.rs
@@ -42,47 +42,43 @@ fn main() {
     let mut res: Vec<Vec<char>> = lines.map(|l| l.chars().collect::<Vec<char>>()).collect();
 
     // search around those indexes to see if there is a number, if there is, return the index of that number
-    let nums: Vec<String> = symbols
+    let nums: i32 = symbols
         .iter()
         .map(|(x, y)| {
-            let mut n: Vec<String> = vec![];
+            let mut n: Vec<i32> = vec![];
             println!("({x}, {y})");
 
             for i in x - 1..x + 2 {
                 for j in y - 1..y + 2 {
                     print!("{}", res[i][j]);
                     if res[i][j].is_numeric() {
-                        let num = extract_num(&mut res[i], j);
+                        let num = extract_num(&mut res[i], j).parse::<i32>().unwrap();
                         // also makes number too low
                         // if !n.contains(&num) {
                         n.push(num)
-                        // }
                     }
                 }
                 println!()
             }
 
-            // makes number too low
-            //n.dedup();
+            // thankfully part 2 requires minimal changes
+            if n.len() == 2 {
+                return n[0] * n[1];
+            }
 
-            n
+            0
         })
-        .flatten()
-        .collect();
-
-    println!("digits: {:?}", nums);
-
-    let num: i32 = nums.iter().map(|x| x.parse::<i32>().unwrap()).sum::<i32>();
+        .sum();
 
     // forward and backward search to extract the number
-    println!("{num}");
+    println!("{nums}");
     ()
 }
 
 fn extract_num(v: &mut Vec<char>, idx: usize) -> String {
     let mut num: String = String::from("");
 
-    let start = v[..idx].iter_mut().rev().map_while(|mut c| {
+    let start = v[..idx].iter_mut().rev().map_while(|c| {
         if c.is_numeric() {
             let d = *c;
             *c = '.';
