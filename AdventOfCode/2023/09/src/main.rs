cef68f2f7d3cdde3113a03fda405c5f3cdb510f1
Tibet Tornaci <oofdere@hey.com>
2023-12-08 22:41:03 -0800

    implementation that should for all intents and purposes be functional but inexplicabily is not giving the right answer

---
+++ AdventOfCode/2023/09/src/main.rs
@@ -0,0 +1,76 @@
+use std::{cmp::Ordering, collections::HashMap, env, fs};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let contents = fs::read_to_string(&args[1]).expect("failed to read file");
+
+    let lines = contents.lines();
+
+    let res = lines
+        .map(|x| {
+            x.split_whitespace()
+                .map(|x| x.parse::<i64>().unwrap())
+                .collect()
+        })
+        .collect::<Vec<Vec<i64>>>();
+
+    println!("{:?}", res);
+
+    let res = res
+        .clone()
+        .into_iter()
+        .map(|x| {
+            let mut vec = vec![x];
+
+            let mut sum = 64;
+            while sum != 0 {
+                let w = vec
+                    .last()
+                    .unwrap()
+                    .windows(2)
+                    .map(|y| y[1] - y[0])
+                    .collect::<Vec<i64>>();
+
+                sum = w.iter().sum();
+
+                vec.push(w);
+            }
+
+            println!("{vec:?}");
+
+            let vec = vec.iter().map(|x| *x.last().unwrap()).collect::<Vec<i64>>();
+
+            vec
+        })
+        .collect::<Vec<Vec<i64>>>();
+
+    println!("{:?}", res);
+
+    let res = res
+        .iter()
+        .map(|seq| {
+            let mut prev = 0;
+            seq.iter()
+                .rev()
+                .map(|x| {
+                    let y = prev + x;
+                    println!("{prev} + {x} = {y}");
+                    prev = y;
+                    y
+                })
+                .collect::<Vec<i64>>()
+        })
+        .collect::<Vec<Vec<i64>>>();
+
+    println!("{:?}", res);
+
+    let res = res.iter().map(|x| *x.last().unwrap()).collect::<Vec<i64>>();
+    println!("aaaaa {:?}", res);
+
+    let res = res.iter().sum::<i64>();
+
+    println!("{:?}", res);
+
+    ()
+}
